ad9cbf9c745383f4e7a36c8bd2a0d68cb1a2ea98
AnkushBhaduri <81172300+AnkushBhaduri@users.noreply.github.com>
2021-04-11 22:37:58 +0530

    Add files via upload

---
+++ scriptiplresults.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-s-BIemmp1YcXkCXGSvOTLajvrjV7d1o",
+    authDomain: "ipl-results-ab923.firebaseapp.com",
+    projectId: "ipl-results-ab923",
+    storageBucket: "ipl-results-ab923.appspot.com",
+    messagingSenderId: "1010412883873",
+    appId: "1:1010412883873:web:e8a98b04e573d300d205e0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
